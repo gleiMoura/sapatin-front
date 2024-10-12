67c75ec2371d6051ab9c6e792e75f9a18745f287
gleiMoura <gle.bar.mou@gmail.com>
2024-10-12 14:20:23 -0300

    ✨ feat: using TopBar component in InicialPage

--- src/pages/InicialPage.tsx
+++ src/pages/InicialPage.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react"
 import styled from "styled-components";
+import { TopBar } from "../components/TopBar";
 
 export const InicialPage: FC = () => {
+    const dataStorage = localStorage.getItem("userInformation");
+
+    const userData = dataStorage ? JSON.parse(dataStorage) : [];
+
     return (
         <Container>
-
+            <TopBar userData={userData} />
+            <Promotion>
+                <div className="image_container" />
+            </Promotion>
         </Container>
     )
 };
@@ -18,4 +26,27 @@ const Container = styled.main`
         text-decoration: none;
         color: #000000
     }
+`;
+
+const Promotion = styled.div`
+    width: 100%;
+    height: 400px;
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+
+    .image_container{
+        width: 100%;
+        height: 100%;
+        background-image: url("./promotion.png"); 
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        
+        @media(max-width: 500px){
+            .image-container{
+                height: 300px;
+            }
+        }
+    };
 `

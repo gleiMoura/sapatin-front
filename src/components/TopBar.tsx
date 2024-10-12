7944b5f993166d2d5a684dd534671e8bf96c30cf
gleiMoura <gle.bar.mou@gmail.com>
2024-10-12 14:18:33 -0300

    ✨ feat: create a header to site called TopBar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { Categories } from "./Categories";
+import styled from "styled-components";
+import { FaShoppingBag } from "react-icons/fa";
+import { FC } from "react";
+import { UserDataType } from "../interfaces";
+
+interface PropsTopBar {
+    userData: UserDataType
+}
+
+export const TopBar: FC<PropsTopBar> = ({ userData }) => {
+    return (
+        <TopBarHeader>
+            <section className="topbar_section">
+                <div className="title">
+                    <Link to={"/"}>sapatin</Link>
+                </div>
+                <div className="bag">
+                    {(userData.name) ? <Link to="/profile">{userData.name}</Link> : <Link to={"/login"}>Entrar</Link>}
+                    <Link to={"/bag"}>
+                        <FaShoppingBag size={30} />
+                    </Link>
+                </div>
+            </section>
+            <Categories />
+        </TopBarHeader>
+    )
+};
+
+const TopBarHeader = styled.header`
+    width: 100%;
+    height: auto;
+    background-color: #ffffff;
+    
+    .topbar_section{
+        width: 100%;
+        height: auto;
+        padding: 20px 40px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #ffffff;
+        box-sizing: border-box;
+        border-bottom: 1px solid darkgray;
+    }
+
+    .title a {
+        font-size: 2rem;
+        font-weight: 700;
+    }
+
+    .bag{
+        width: 150px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .bag a{
+        font-size: 1.2rem;
+        font-weight: 500;
+    }
+`
+

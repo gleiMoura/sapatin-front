32f7a25469268d5a4469263ec62a3223717915a0
gleiMoura <gle.bar.mou@gmail.com>
2024-10-12 17:17:26 -0300

    ✨ feat: create a Footer component to Incial Page

--- src/pages/InicialPage.tsx
+++ src/pages/InicialPage.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react"
 import styled from "styled-components";
 import { TopBar } from "../components/TopBar";
+import { CgAdidas } from "react-icons/cg";
+import { SiNike } from "react-icons/si";
 
 export const InicialPage: FC = () => {
     const dataStorage = localStorage.getItem("userInformation");
@@ -13,6 +15,22 @@ export const InicialPage: FC = () => {
             <Promotion>
                 <div className="image_container" />
             </Promotion>
+            <Footer>
+                <div className="main">
+                    <h1>Procurando por uma marca?</h1>
+                    <div className="brands">
+                        <div className="brand">
+                            <SiNike size={50} />
+                        </div>
+                        <div className="brand">
+                            <CgAdidas size={50} />
+                        </div>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <p>Produzido por Gleison Moura</p>
+                </div>
+            </Footer>
         </Container>
     )
 };
@@ -49,4 +67,63 @@ const Promotion = styled.div`
             }
         }
     };
+`;
+
+const Footer = styled.div`
+    width: 100%;
+    height: auto;
+    margin-top: 30px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    
+    .main{
+        height: 100%;
+        padding: 15px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid darkgray
+    }
+
+    .main h1{
+        padding: 15px;
+        box-sizing: border-box;
+        font-size: 1.5rem;
+        text-align: center;
+        font-weight: 700;
+    }
+    
+    .brands{
+        height: 100%;
+        padding: 15px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .brand{
+        width: 180px;
+        height: 180px;
+        border-radius: 50%;
+        border: 1px solid darkgray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        color: white;
+        background-color: black;
+    };
+
+    .bottom{
+        width: 100%;
+        height: 40px;
+        background-color: white;
+        padding: 10px;
+        box-sizing: border-box;
+        text-align: center;
+        font-size: 0.8rem;
+    }
 `

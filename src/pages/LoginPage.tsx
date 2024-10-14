35e086d4c16df6f87317c0f6caaa61b2b0f04d63
gleiMoura <gle.bar.mou@gmail.com>
2024-10-14 17:44:21 -0300

    ✨ finishing login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,51 +1,54 @@
-import { ChangeEvent, FC, InputHTMLAttributes, ReactNode, useState } from "react";
+import { ChangeEvent, FC, useState } from "react";
+import { ThreeDots } from 'react-loader-spinner';
+import { useNavigate, Link } from "react-router-dom";
 import { StaticPage } from "../components/StaticPage";
 import styled from "styled-components";
+import { CredentialsType } from "../interfaces";
+import doLogin from "../services/doLogin";
 
 export const LoginPage: FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loadButton, setLoadButton] = useState(true);
+    const navigate = useNavigate();
 
-    const getEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleGetEmail = (e: ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
     };
 
-    const getPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleGetPassword = (e: ChangeEvent<HTMLInputElement>) => {
         setPassword(e.target.value);
     };
+
+    const handleDoLogin = async () => {
+        const credentials: CredentialsType = { email, password };
+        if (!email || !password) {
+            alert("Preencha os dois campos!");
+            return
+        }
+
+        try {
+            setLoadButton(false);
+            await doLogin(credentials);
+            navigate("/");
+        } catch (error) {
+            console.log("Erro ao tentar fazer login!", error);
+            console.error(error);
+        } finally {
+            setLoadButton(true)
+        }
+    }
 
     return (
         <StaticPage children={
             <LoginContainer>
                 <form>
-                    <input type="email" id='email' placeholder='email' required onChange={getEmail} />
-                    <input type="password" id="password" placeholder='senha' required onChange={getPassword} />
+                    <input type="email" id='email' placeholder='email' required onChange={handleGetEmail} />
+                    <input type="password" id="password" placeholder='senha' required onChange={handleGetPassword} />
 
-                    <button className={loadButton ? "" : "hide"} onClick={(e) => {
-                        if (!email || !password) {
-                            alert("Preencha os dois campos!");
-                            navigate("/");
-                        } else {
-                            setLoadButton(false)
-                            const requestion = axios.post("https://sapatin.onrender.com/login", {
-                                email: email,
-                                password: password
-                            });
-                            requestion.then(answer => {
-                                setData(answer.data);
-                                navigate("/")
-                            })
-                            requestion.catch(err => {
-                                alert("dados inválidos!", err.data);
-                                setLoadButton(true);
-                                navigate("/login");
-                            })
-                        }
-                        e.preventDefault();
-                    }}>Entrar</button>
+                    <button className={loadButton ? "" : "hide"} onClick={handleDoLogin}>Entrar</button>
 
-                    <button className={loadbutton ? "hide" : "loading"}>
+                    <button className={loadButton ? "hide" : "loading"}>
                         <ThreeDots
                             height="80"
                             width="80"
@@ -62,7 +65,7 @@ export const LoginPage: FC = () => {
             </LoginContainer>
         } />
     )
-};
+}
 
 const LoginContainer = styled.div`
     width: 100%;

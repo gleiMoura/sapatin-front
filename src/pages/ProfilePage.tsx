e385a309fca60904f4e9a15dfc4ee04d67535fa0
gleiMoura <gle.bar.mou@gmail.com>
2024-10-16 02:07:18 -0300

    ✨💄 create profile page and the style to it

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,26 @@
 import styled from "styled-components";
 import { StaticPage } from "../components/StaticPage";
+import { getByStorage } from "../hooks/useLocalStorage";
+import { useState } from "react";
+import { AdressForm } from "../components/AdressForm";
 
 export const ProfilePage = () => {
-    return (
-        <StaticPage children={
-            <ProfileContainer>
+    const { adress } = getByStorage("userData");
+    const [popUp, setPopUp] = useState<boolean>(false);
 
+    const handleOpenPopUp = () => {
+        setPopUp(true);
+    }
+    return (
+        <StaticPage withEnter children={
+            <ProfileContainer>
+                <AdressForm popUp={popUp} setPopUp={setPopUp} />
+                <div className="data">
+                    <div className="adress">
+                        <p>{adress ? adress : "Você ainda não possui um endereço cadastrado!"}</p>
+                    </div>
+                    <button onClick={handleOpenPopUp}>{adress ? "Mudar endereço" : "cadastrar endereço"}</button>
+                </div>
             </ProfileContainer>
         } />
     )
@@ -16,5 +31,46 @@ const ProfileContainer = styled.div`
     height: 400px;
     margin-top: 30px;
     display: flex;
-    justify-content: center;
+
+    .data{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .adress{
+        width: auto;
+        height: 50px;
+        padding: 20px;
+        box-sizing: border-box;
+        font-size: 1rem;
+        font-weight: 600;
+        text-align: center;
+        background-color: black;
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-right: 1px solid darkgray;
+    }
+
+    button{
+        width: auto;
+        height: 50px;
+        padding: 20px;
+        box-sizing: border-box;
+        font-size: 1rem;
+        font-weight: 600;
+        text-align: center;
+        background-color: white;
+        color: black;
+        border: none;
+        cursor: pointer;
+        border: 1px solid black;
+        box-sizing: border-box;
+    }
+
+    button:hover{
+        background-color: black;
+        color:white;
+    }
 `;

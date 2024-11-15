df9cf847b1b9519e01330a08151d4983b59bbfb9
gleiMoura <gle.bar.mou@gmail.com>
2024-11-15 18:06:43 -0300

    🏷️ put an adress type

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -7,3 +7,12 @@ export interface CredentialsType {
     email: string,
     password: string
 };
+
+export interface AdressType {
+    street: string,
+    number: number | string,
+    city: string,
+    state: string,
+    cep: string,
+    more?: string
+}
